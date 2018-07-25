2a5c41e81b7e350981222835a0a99845ac8fbd69
80prozent <80prozent@differentdesign.de>
2018-07-25 15:38:04 +0000

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.log("AwayJS - Graphics - 0.2.41");
+console.log("AwayJS - Graphics - 0.2.42");
 export {AnimationElements}				from "./lib/animators/data/AnimationElements";
 export {ColorSegmentPoint}				from "./lib/animators/data/ColorSegmentPoint";
 export {JointPose}						from "./lib/animators/data/JointPose";
