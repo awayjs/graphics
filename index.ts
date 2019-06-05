dff1b3a2d83044edaab7b5d132583d56f0174954
Rob Bateman <rob.bateman@gmail.com>
2019-06-05 16:17:30 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.log("AwayJS - Graphics - 0.4.1");
+console.log("AwayJS - Graphics - 0.4.2");
 export {AnimationElements}				from "./lib/animators/data/AnimationElements";
 export {ColorSegmentPoint}				from "./lib/animators/data/ColorSegmentPoint";
 export {JointPose}						from "./lib/animators/data/JointPose";
