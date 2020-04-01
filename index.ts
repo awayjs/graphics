52b1cb79b0731794e9741e502044b147195d0fa4
eXponenta <rondo.devil@gmail.com>
2020-04-01 19:48:04 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.debug("AwayJS - Graphics - 0.5.3");
+console.debug("AwayJS - Graphics - 0.5.4");
 export {AnimationElements}				from "./lib/animators/data/AnimationElements";
 export {ColorSegmentPoint}				from "./lib/animators/data/ColorSegmentPoint";
 export {JointPose}						from "./lib/animators/data/JointPose";
