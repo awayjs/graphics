0b7d6ff56de2d8e9935bfa20bfdb0c9eeb15e4ea
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-04-29 14:36:11 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug('AwayJS - Graphics - 0.5.59');
+console.debug('AwayJS - Graphics - 0.5.60');
 export { AnimationElements } from './lib/animators/data/AnimationElements';
 export { ColorSegmentPoint } from './lib/animators/data/ColorSegmentPoint';
 export { JointPose } from './lib/animators/data/JointPose';
