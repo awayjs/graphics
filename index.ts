b57053062b62e4557c07517ad6f74da1cf370d98
nosamu <71368227+n0samu@users.noreply.github.com>
2023-12-11 21:20:37 -0600

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug('AwayJS - Graphics - 0.5.82');
+console.debug('AwayJS - Graphics - 0.5.83');
 export { AnimationElements } from './lib/animators/data/AnimationElements';
 export { ColorSegmentPoint } from './lib/animators/data/ColorSegmentPoint';
 export { JointPose } from './lib/animators/data/JointPose';
